899430b18ed36735d709f7fc53e5156e66158e16
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-12-04 16:31:25 -0800

    created student class, declared member variables and added function prototypes

---
+++ student.h
@@ -0,0 +1,21 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+
+class student
+{
+    private:
+    string firstName;
+    string lastName;
+
+    public:
+    void person.setName(string, string);
+    void person.fullName();
+
+};
+
+
+
+
+
+
+#endif
